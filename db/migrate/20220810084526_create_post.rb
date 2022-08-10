24f6a47c8d1dfeea408291ce34c87c8c0f9c8f30
yasin <yasin.elsharawi23@gmail.com>
2022-08-10 14:29:33 +0300

    Update column naming to snake_case and Update Users relation to be author_id

--- db/migrate/20220810084526_create_post.rb
+++ db/migrate/20220810084526_create_post.rb
@@ -1,11 +1,11 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true, index: true
+      t.references :author, null: false, foreign_key: {to_table: 'users'}, index: true
       t.string :title
       t.text :text
-      t.integer :commentsCounter
-      t.integer :likesCounter
+      t.integer :comment_counter
+      t.integer :like_counter
 
       t.timestamps
     end
