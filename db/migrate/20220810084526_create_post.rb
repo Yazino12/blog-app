9c76a3103eb04b2efe5d5d6895d454ce88adab89
yasin <yasin.elsharawi23@gmail.com>
2022-08-10 13:07:09 +0300

    Create a data model Setup project with postgresql Create migration files Migrate databse with bin/rails db:migrate and update db/schema.rb

---
+++ db/migrate/20220810084526_create_post.rb
@@ -0,0 +1,13 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.references :user, null: false, foreign_key: true, index: true
+      t.string :title
+      t.text :text
+      t.integer :commentsCounter
+      t.integer :likesCounter
+
+      t.timestamps
+    end
+  end
+end
