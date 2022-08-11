a5e6b53c4293caa1062ee313dca96ffd741929b5
yasin <yasin.elsharawi23@gmail.com>
2022-08-12 01:00:16 +0300

    Run Rubocop and resolve errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    def update_like_counter
-        post.increment!(:like_counter)
-    end
+  def update_like_counter
+    post.increment!(:like_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :likes
-    has_many :comments
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
 
-    def update_post_counter
-        author.increment!(:post_counter)
-    end
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
 
-    def recent_comments
-        comments.limit(5).order(created_at: :desc)
-    end
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
