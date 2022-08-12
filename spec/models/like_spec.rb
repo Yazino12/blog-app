e6d0b6f5069443bf55335ed4cc57203bfbe1a1ec
yasin <yasin.elsharawi23@gmail.com>
2022-08-12 20:55:23 +0300

    Add unit specs for like model methods and validations

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                           post_counter: 0)
+
+  first_post = first_user.posts.create(title: 'Hello', text: 'This is my first post', comment_counter: 0,
+                                       like_counter: 0)
+
+  first_post.likes.create(author_id: first_user.id, post_id: first_post.id)
+
+  it 'like_counter for Post should be updated' do
+    expect(first_post.like_counter).to eq(1)
+  end
 end
