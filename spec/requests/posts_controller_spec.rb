088572710e085786a57838d0b99e6566702a148f
yasin <yasin.elsharawi23@gmail.com>
2022-08-16 16:37:33 +0300

    Create Request spec for posts_controller Check if response status was correct Check if correct template is rendered Check if the response body includes correct placeholder text

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'User management', type: :request do
+RSpec.describe 'Post management', type: :request do
   it 'check actions for home route' do
-    get '/'
+    get '/users/1/posts'
     expect(response).to have_http_status(:success)
     expect(response).to render_template(:index)
-    expect(response.body).to include('ALL USERS')
+    expect(response.body).to include('HERE ARE ALL POSTS FOR A GIVEN USER')
   end
 
   it 'check actions for /users/ID route' do
-    get '/users/1'
+    get '/users/1/posts/3'
     expect(response).to have_http_status(:success)
     expect(response).to render_template(:show)
-    expect(response.body).to include('HERE IS A GIVEN USER BY ID')
+    expect(response.body).to include('HERE IS A POST BY ID FOR A GIVEN USER')
   end
 end
