f5bf14f617d6957f7bddc12628ff170fabadb712
yasin <yasin.elsharawi23@gmail.com>
2022-08-16 17:00:48 +0300

    Correct it description for posts_controller_spec

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Post management', type: :request do
-  it 'check actions for home route' do
+  it 'check actions for /users/ID/posts route' do
     get '/users/1/posts'
     expect(response).to have_http_status(:success)
     expect(response).to render_template(:index)
     expect(response.body).to include('HERE ARE ALL POSTS FOR A GIVEN USER')
   end
 
-  it 'check actions for /users/ID route' do
+  it 'check actions for /users/ID/posts/ID route' do
     get '/users/1/posts/3'
     expect(response).to have_http_status(:success)
     expect(response).to render_template(:show)
